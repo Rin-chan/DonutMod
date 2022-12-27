ffcc96f4217bfbaff61b028a3099d50a5e8d97a1
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-12-27 11:50:14 +0800

    Clean up unused imports

--- src/main/java/github/rin_chan/donutmod/item/SimpleMochiItem.java
+++ src/main/java/github/rin_chan/donutmod/item/SimpleMochiItem.java
@@ -1,8 +1,6 @@
 package github.rin_chan.donutmod.item;
 
 import github.rin_chan.donutmod.util.ModItemGroup;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 

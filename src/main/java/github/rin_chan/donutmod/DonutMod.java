d659c86b729d7a5eeae2b26aa806e2eb22553ca9
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-12-27 12:39:44 +0800

    Remove unused event

--- src/main/java/github/rin_chan/donutmod/DonutMod.java
+++ src/main/java/github/rin_chan/donutmod/DonutMod.java
@@ -24,15 +24,4 @@ public class DonutMod {
         
         ItemRegistry.init();
     }
-    
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents
-    {
-    	@SubscribeEvent
-	    public static void onEntityAttributeModificationEvent(final EntityAttributeModificationEvent event) {
-			event.add(EntityType.PLAYER, ForgeMod.REACH_DISTANCE.get());
-		}
-    }
 }
